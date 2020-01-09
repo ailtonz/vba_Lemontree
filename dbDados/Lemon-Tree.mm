8be76a4283b5c2d77fd52ed4dd1a770519acf0c4
Ailton Zacarias <ailton@f4consult.com.br>
2020-01-09 20:14:04 -0300

    Add files via upload

---
+++ dbDados/Lemon-Tree.mm
@@ -0,0 +1,103 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1270037237673" ID="Freemind_Link_1371365829" MODIFIED="1270037247325" TEXT="Lemon-Tree">
+<node CREATED="1270037248369" FOLDED="true" ID="_" MODIFIED="1270561317793" POSITION="right" TEXT="Vendas">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1270037255739" ID="Freemind_Link_43647727" MODIFIED="1270561311689" TEXT="Sele&#xe7;&#xe3;o de Produtos Acabados">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1270037278074" ID="Freemind_Link_462311568" MODIFIED="1270561284778" TEXT="Mudar Compra p/ ok">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1270037296544" ID="Freemind_Link_1673398106" MODIFIED="1270561293473" TEXT="Formas de Pagamento">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1270037313988" ID="Freemind_Link_1465681184" MODIFIED="1270561298050" TEXT="Ponto de Venda">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1270037324744" ID="Freemind_Link_1111312127" MODIFIED="1270561305016" TEXT="Valores">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1270037332453" ID="Freemind_Link_1229273671" MODIFIED="1270037340169" TEXT="Sugest&#xe3;o"/>
+<node CREATED="1270037341809" ID="Freemind_Link_1836239971" MODIFIED="1270037343867" TEXT="Pago"/>
+</node>
+</node>
+<node CREATED="1270037357881" ID="Freemind_Link_1362916264" MODIFIED="1270037362158" POSITION="left" TEXT="Produtos">
+<node CREATED="1270037364147" ID="Freemind_Link_958361112" MODIFIED="1270037370135" TEXT="Tamanhos">
+<node CREATED="1270037380810" ID="Freemind_Link_1817249960" MODIFIED="1270037402176" TEXT="Custo da Mat&#xe9;ria Prima"/>
+<node CREATED="1270558578549" ID="Freemind_Link_1289100519" MODIFIED="1270558593386" TEXT="Grades">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1270037371550" ID="Freemind_Link_112302257" MODIFIED="1270037374055" TEXT="Cores">
+<node CREATED="1270558540804" ID="Freemind_Link_80834762" MODIFIED="1270558556931" TEXT="Listagem de sele&#xe7;&#xe3;o de cores"/>
+</node>
+<node CREATED="1270037925046" ID="Freemind_Link_751293377" MODIFIED="1270558787484" TEXT="Listagem Sele&#xe7;&#xe3;o de Cores"/>
+</node>
+<node CREATED="1270037411698" ID="Freemind_Link_1627295211" MODIFIED="1270037416226" POSITION="left" TEXT="Produ&#xe7;&#xe3;o">
+<node CREATED="1270037421675" ID="Freemind_Link_1073887884" MODIFIED="1270037448174" TEXT="Tela Igual ao modelo excel">
+<node CREATED="1270037451297" ID="Freemind_Link_1174694119" MODIFIED="1270037457002" TEXT="Tamanhos"/>
+<node CREATED="1270037460623" ID="Freemind_Link_535583846" MODIFIED="1270037478088" TEXT="Artigos/Modelos/Cores"/>
+</node>
+</node>
+<node CREATED="1270039533141" FOLDED="true" ID="Freemind_Link_1328739410" MODIFIED="1270561274904" POSITION="left" TEXT="Etiquetas">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1270039558938" ID="Freemind_Link_811299723" MODIFIED="1270558153478" TEXT="Repositorio">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1270558606405" ID="Freemind_Link_1275630542" MODIFIED="1270558646305" TEXT="Gerar Planilha para C&#xf3;digo de Barras">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1270558697669" ID="Freemind_Link_1903160055" MODIFIED="1270558709764" TEXT="Excluir c&#xf3;digo de Barras">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1270558714956" ID="Freemind_Link_747937093" MODIFIED="1270561268214" TEXT="Listagem de sele&#xe7;&#xe3;o de produtos em estoque">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1270037489058" ID="Freemind_Link_724981771" MODIFIED="1270037494224" POSITION="left" TEXT="Estoque">
+<node CREATED="1270037977694" ID="Freemind_Link_452813088" MODIFIED="1270037981411" TEXT="Controles">
+<node CREATED="1270037499111" ID="Freemind_Link_1077835318" MODIFIED="1270037523814" TEXT="(+) Tecido = Despesa"/>
+<node CREATED="1270037538806" ID="Freemind_Link_1217522163" MODIFIED="1270037556571" TEXT="(-) Produtos Costurados"/>
+<node CREATED="1270037526735" ID="Freemind_Link_84863431" MODIFIED="1270037537139" TEXT="(-) Produtos Cortados"/>
+<node CREATED="1270037558389" ID="Freemind_Link_1418726829" MODIFIED="1270037564645" TEXT="(-) Vendas"/>
+</node>
+<node CREATED="1270038008371" ID="Freemind_Link_805837592" MODIFIED="1270038019958" TEXT="Entrada de Mat&#xe9;ria Prima">
+<node CREATED="1270038020903" ID="Freemind_Link_124048503" MODIFIED="1270038025197" TEXT="Tecido"/>
+<node CREATED="1270038027313" ID="Freemind_Link_1604659385" MODIFIED="1270038029495" TEXT="Cor"/>
+<node CREATED="1270038031728" ID="Freemind_Link_136734099" MODIFIED="1270038036578" TEXT="Quilos"/>
+<node CREATED="1270038038932" ID="Freemind_Link_1946018942" MODIFIED="1270038041143" TEXT="Metros"/>
+<node CREATED="1270038043774" ID="Freemind_Link_1177586776" MODIFIED="1270038084308" TEXT="Valor Metro(s) - Valor Quilo/Rendimento por Metro"/>
+<node CREATED="1270038089479" ID="Freemind_Link_278778119" MODIFIED="1270038095292" TEXT="Valor Quilo"/>
+<node CREATED="1270038098024" ID="Freemind_Link_776951655" MODIFIED="1270038129966" TEXT="Rendimento por Metro(s) - (Metros/Quilos)"/>
+</node>
+</node>
+<node CREATED="1270037762266" ID="Freemind_Link_824707857" MODIFIED="1270037776540" POSITION="right" TEXT="Saidas (Arquivos)">
+<node CREATED="1270037680926" ID="Freemind_Link_1710891008" MODIFIED="1270037686559" TEXT="C&#xf3;digo de Barras">
+<node CREATED="1270037687652" ID="Freemind_Link_664846332" MODIFIED="1270037715681" TEXT="Gerar Planilha de dados para etiquetas"/>
+<node CREATED="1270037946446" ID="Freemind_Link_678193047" MODIFIED="1270037965066" TEXT="Adicionar Valor Unit&#xe1;rio"/>
+</node>
+<node CREATED="1270037797987" ID="Freemind_Link_779953617" MODIFIED="1270037802975" TEXT="Recebimentos">
+<node CREATED="1270037804411" ID="Freemind_Link_1515136350" MODIFIED="1270037858646" TEXT="Gerar Planilha de vendas do Ponto"/>
+</node>
+<node CREATED="1270037861483" ID="Freemind_Link_1185388034" MODIFIED="1270037864745" TEXT="Vendas">
+<node CREATED="1270037865906" ID="Freemind_Link_1257089658" MODIFIED="1270037885596" TEXT="Gerar Planilha sobre as vendas realizadas"/>
+</node>
+</node>
+<node CREATED="1270038209193" ID="Freemind_Link_1921204792" MODIFIED="1270038215593" POSITION="left" TEXT="Financeiro">
+<node CREATED="1270038216696" ID="Freemind_Link_291839442" MODIFIED="1270038226644" TEXT="Receitas"/>
+<node CREATED="1270038228631" ID="Freemind_Link_1225266240" MODIFIED="1270038233126" TEXT="Despesas"/>
+</node>
+<node CREATED="1270039587366" ID="Freemind_Link_482228230" MODIFIED="1270039589667" POSITION="right" TEXT="ADM">
+<node CREATED="1270037593069" ID="Freemind_Link_1697930375" MODIFIED="1270037596647" TEXT="Categorias">
+<node CREATED="1270037598422" ID="Freemind_Link_1184677433" MODIFIED="1270037618940" TEXT="N&#xe3;o Permitir c&#xf3;digo Repetido"/>
+<node CREATED="1270037625851" ID="Freemind_Link_508049372" MODIFIED="1270037635084" TEXT="Categorias">
+<node CREATED="1270037636354" ID="Freemind_Link_232735498" MODIFIED="1270037640282" TEXT="Artigos"/>
+<node CREATED="1270037641857" ID="Freemind_Link_629059981" MODIFIED="1270037658642" TEXT="Composi&#xe7;&#xf5;es"/>
+<node CREATED="1270037650476" ID="Freemind_Link_523033245" MODIFIED="1270037654595" TEXT="Modelos"/>
+<node CREATED="1270037663654" ID="Freemind_Link_1542800675" MODIFIED="1270037665838" TEXT="Cores"/>
+<node CREATED="1270037668310" ID="Freemind_Link_761983844" MODIFIED="1270037670898" TEXT="Tamanhos"/>
+</node>
+</node>
+</node>
+</node>
+</map>
